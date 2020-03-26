3186a0c047684b700d21be9828b8158211c3ddb4
mariam <avanessm@outlook.fr>
2020-03-26 17:18:02 +0100

    hooks

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,50 @@
 import React, { Fragment, Component, useState, useContext } from "react";
 import { useHistory } from "react-router-dom";
 
+const monContext = createContext("coucou");
+
 export const Form = props => {
   const [state, setState] = useState({ nom: "", age: 0 });
   const history = useHistory();
+  const context = useContext(monContext);
+
+  //componentDidMount
+  useEffect(() => {
+    // traitement 1
+  }, []);
+
+  //componentWillUnmount
+  useEffect(() => {
+    return () => {
+      //traitement 3
+    };
+  }, []);
+
+  //componentDidMount + componentWillUnmount
+  useEffect(() => {
+    // traitement 1
+    let subscr = observable.subscribe();
+    return () => {
+      subscr.unsubscribe();
+    };
+  }, []);
+
+  // componentDidUpdate
+  useEffect(() => {
+    //traitement 2
+  });
+
+  // componentDidUpdate + shouldComponentUpdate
+  useEffect(() => {
+    //traitement 2
+  }, [state.nom]); // ou [setState] ou [state.nom, state.age]
 
   const handleName = event => {
     setState({ ...state, nom: event.target.value });
   };
 
   navigate = () => {
+    console.log(context);
     history.location.push("/url");
   };
 
--- src/components/FormComp.jsx
+++ src/components/FormComp.jsx
@@ -1,4 +1,6 @@
-import React, { Component } from "react";
+import React, { Component, createContext } from "react";
+
+const monContext = createContext("coucou");
 
 export class FormComp extends Component {
   state = {
@@ -6,20 +8,48 @@ export class FormComp extends Component {
     age: 0
   };
 
+  constructor() {
+    super(props);
+  }
+  componentDidMount() {
+    // traitement 1
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.nom !== nextState.nom) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  componentDidUpdate() {
+    // traitement 2
+  }
+
+  componentWillUnmount() {
+    // traitement 3
+  }
+
   handleName = event => {
     this.setState({ nom: event.target.value });
   };
 
-  navigate = () => {
+  navigate = context => {
+    console.log(context);
     this.props.history.location.push("/url");
   };
 
   render() {
     return (
-      <Fragment>
-        <input type="text" onChange={handleName} value={this.state.nom} />
-        <button onClick={navigate}>Nav</button>
-      </Fragment>
+      <monContext.Consumer>
+        {value => (
+          <Fragment>
+            <input type="text" onChange={handleName} value={this.state.nom} />
+            <button onClick={() => navigate(data)}>Nav</button>
+          </Fragment>
+        )}
+      </monContext.Consumer>
     );
   }
 }
